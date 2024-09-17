9fffccfa254333a394a6f359aa4efd0b4129410e
ws7465 <ws7465@ya.ru>
2024-09-18 00:46:38 +0300

    main.py - 20240918 00:47

--- main.py
+++ main.py
@@ -31,3 +31,20 @@ x = 43
 y = 32
 print(x * y)
 print("End line")
+
+
+
+
+while True :
+    a = input('введите целое :')
+    if a.isdigit() != 1 :
+        print('не число')
+        break
+    if int(a) % 2 == 0 :
+        print('число чётное')
+        continue
+        print('я не попадаю в цикл')
+    else :
+        print('число нечётное')
+print('я за циклом')
+
